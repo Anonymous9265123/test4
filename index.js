57d7ebc42291d8ad82d3bf55f3d3a9d7fac3f42a
Anonymous9265123 <henrylord420@gmail.com>
2024-08-02 15:16:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const MONGO_URI = process.env.MONGODB_URI;
 
